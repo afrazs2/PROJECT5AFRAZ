b954752f165a7eaa68681eb0dff8a63ffad0f7b3
Afraz Samir <afrazs2@nycstudents.net>
2026-04-27 08:43:12 -0400

    Initial commit

---
+++ src/DrawPanel.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import java.util.ArrayList;
+
+public class DrawPanel extends JPanel {
+
+    public DrawPanel()
+    {
+
+
+    }
+
+    protected void paintComponent(Graphics g) {
+       int[][] grid = new int[30][40];
+        for(int r = 0; r < 30; r ++)
+        {
+            for(int c = 0; c < 40; c++)
+            {
+
+            }
+        }
+    }
+
+}
