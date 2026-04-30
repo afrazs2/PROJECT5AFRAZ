be627672ee0f57a7d33040d2d9b99a499dc8815a
afrazs2 <afrazs2@nycstudents.net>
2026-04-30 08:38:19 -0400

    Add files via upload

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -13,12 +13,13 @@ public class DrawPanel extends JPanel {
     }
 
     protected void paintComponent(Graphics g) {
-       int[][] grid = new int[30][40];
-        for(int r = 0; r < 30; r ++)
-        {
-            for(int c = 0; c < 40; c++)
-            {
+        super.paintComponent(g);
 
+
+
+        for (int r = 0; r < 30; r++) {
+            for (int c = 0; c < 40; c++) {
+                g.drawRect(c * (24), r * (24), 20, 20);
             }
         }
     }
